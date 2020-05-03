48f32e88e317b47429a3e68a51a717346e5048c1
Mbugua Kabage <mkabage@gmail.com>
2020-05-03 14:03:49 +0200

    implement the methods to allow the tests to pass

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,11 +7,48 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
   end
 
+  def guess(word)
+    word.downcase! unless word.nil?
+    
+    raise ArgumentError.new("Expected a letter") if invalid?(word)
+
+    return false  if repeated_word?(word)
+    if (@word.include? word)
+      @guesses += word
+    else
+      @wrong_guesses += word
+    end
+    true
+  end
+
+  def check_win_or_lose
+    if (@word.chars.sort == @guesses.chars.sort)
+      :win
+    elsif @wrong_guesses.size == 7
+      :lose
+    else
+      :play
+    end
+  end
+
+  def word_with_guesses
+    redacted = @word.chars.map do |char|
+      char = @guesses.include?(char) ? char : '-' 
+    end
+    redacted.join('')
+  end
+ 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
@@ -24,4 +61,13 @@ class HangpersonGame
     }
   end
 
+  private
+
+  def invalid?(word)
+    word.nil? || word.ord < 97 || word.ord > 122
+  end
+
+  def repeated_word?(word)
+    @guesses.include?(word) || @wrong_guesses.include?(word) || word.nil?
+  end
 end
